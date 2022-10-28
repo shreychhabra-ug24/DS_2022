e345008f447a97fa3bba11cd7cbe931187fbaac0
shreychhabra-ug24 <shrey.chhabra_ug24@ashoka.edu.in>
2022-10-28 22:38:24 +0530

    first few C files

---
+++ mult_matrix.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main() 
+{
+    int m, n, sum= 0;
+    int a[3][4], b[4][2], result[3][2]; 
+    printf("Enter 1st matrix\n");
+    for (int i =0; i<3, i++)
+    {
+        for (int j=0, j < 4, j++)
+        {
+            //printf("enter the %d %d element of 1st matrix", i, j);
+            scanf("%d", &a[i][j]);
+            //printf("\t");
+
+        }
+        //printf("\n");
+    }
+    printf("Enter the second matrix\n");
+    for(int i = 0, i<4, i++)
+    {
+        for (int j= 0; )
+    }
+
+
+}
