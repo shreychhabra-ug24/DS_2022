6ed4cbb85571c3c3e0a5d12cf687c38ff5610f6a
shreychhabra-ug24 <shrey.chhabra_ug24@ashoka.edu.in>
2022-11-12 15:37:05 +0530

    binary search tree deletion

---
+++ bst_deletion.c
@@ -0,0 +1,12 @@
+struct node *remove(struct node *root, int key)
+{
+	if(root==NULL) return root;
+
+	if (key<root->key)
+		root->left = remove(root->left, key);
+	else if (key>root->key)
+		root->right = remove(root->right, key);
+	else{
+		
+	}
+}
