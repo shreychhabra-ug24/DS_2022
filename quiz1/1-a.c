c14b75345d9321cb4ad83fec0c302ad8b9e68666
shreychhabra-ug24 <shrey.chhabra_ug24@ashoka.edu.in>
2022-11-15 23:38:29 +0530

    1-a from quiz1

--- quiz1/1-a.c
+++ quiz1/1-a.c
@@ -1,20 +1,24 @@
 //fizzbuzz program
 #include <stdio.h>
 
+void basicFizzBuzz(int x){
+    
+    if(x%3==0||x%5==0){
+        printf("fizz\n");
+    }
+    if(x%11==0||x%13==0){
+        printf("buzz\n");
+    }
+    if(x%33==0||x%39==0||x%55==0||x%65==0){
+        printf("fizzbuzz\n");
+    }
+    else(printf("%d"));
+}
+
 int main(){
     int x;
     printf("enter the integer: ");
     scanf("%d", &x);
-    if (x%3==0, x%5==0){
-        printf("fizz");
-    }
-    else if (x%11==0, x%13==0){
-        printf("buzz");
-    }
-    else if(x%33==0,x%39==0, x%55==0, x%65==0){
-        printf("fizzbuzz");
-    }
-    
-    return 0;
+    basicFizzBuzz(x);
 }
 
