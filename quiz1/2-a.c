ee2a5f7fab6f570e594ccb8b2fb7bfa8b5c18e2f
shreychhabra-ug24 <shrey.chhabra_ug24@ashoka.edu.in>
2022-11-17 12:55:30 +0530

    2-a.c

--- quiz1/2-a.c
+++ quiz1/2-a.c
@@ -1,30 +1,34 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <time.h>
 
 struct node{
     int val;
     struct node*next;
     struct node*next2;
 };
+typedef struct node * NodeAddress;
 
-int main(){
-    int a[] = {1,2,3,4,5};
-    struct node* head = NULL;
-    struct node* n2 = NULL;
-    struct node* n3 = NULL;
+NodeAddress arrayToLLWithNext2(int *a, int n){
+    NodeAddress head = NULL;
+    NodeAddress n2 = NULL;
+    NodeAddress n3 = NULL;
+    int x;
 
     head = (struct node*)malloc(sizeof(struct node));
     n2 = (struct node*)malloc(sizeof(struct node));
     n3 = (struct node*)malloc(sizeof(struct node));
+if(n>0){
+        head->val = a[0];
+        head->next = n2;
+        head->next2 = n3;
+        n2 = head;
+        n3 = head;
+    }
 
-    head->val = a[0];
-    head->next = n2;
-    head->next2 = n3;
-
-    n2->val = a[1];
-    n2->next = n3;
-    n2->next2 = NULL;
+        n2->val = a[1];
+        n2->next = n3;
+        n2->next2 = NULL;
+        }
 
     n3->val = a[2];
     n3->next = NULL;
@@ -33,4 +37,3 @@ int main(){
     return 0;
 
 }
-
