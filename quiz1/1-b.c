a414f7edc78b3e05cba723d57d1d0911106f94ae
shreychhabra-ug24 <shrey.chhabra_ug24@ashoka.edu.in>
2022-11-17 12:38:38 +0530

    1-b.c

---
+++ quiz1/1-b.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+void advancedFizzBuzz(int *a, int m, int *b, int nb, int *f, int nf){
+    int f[] = {3,5};
+    int b[] = {11,13};
+    nf = 2;
+    nb = 2;
+    printf("enter the length of the input array: ");
+    scanf("%d",m);
+    int a[] = {};
+    m = sizeof(a);
+
+    printf("Enter the array inputs: ");
+    for(int i=0; i<m; i++){
+        scanf("%d", &a[i]);
+    }
+    
+
+}
